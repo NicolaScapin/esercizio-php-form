1cf53c738478e93b9747fe2c2e2fdea81578417f
NicolaScapin <44172919+NicolaScapin@users.noreply.github.com>
2019-03-20 10:10:00 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php
+
+$nome = (isset($_POST['nome'])) ? $_POST['nome']:'';
+$cognome = (isset($_POST['cognome'])) ? $_POST['cognome']:'';
+$indirzzo = (isset($_POST['indirizzo'])) ? $_POST['indirizzo']:'';
+
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -12,19 +20,39 @@
   </head>
   <body> 
     <div class="container-fluid">
-      <h1>Inserisci i tuoi dati</h1>
+      <div class="alert alert-primary" role="alert">
+        <?php
+          foreach ($_POST as $key => $value) {
+            print $key . ': ' . $value . '<br>';
+          }
+        ?>
+      </div>
+        <h1>Inserisci i tuoi dati</h1>
         
-        <form>
-          <div class="form-group">
-            <label for="name">Nome</label>
-            <input type="text" class="form-control" id="name" aria-describedby="nameHelp" placeholder="Il tuo nome">
-            <small id="nameHelp" class="form-text text-muted">Inserisci il tuo nome</small>
-          </div>
-          <div class="form-group">
-            <label for="surname">Cognome</label>
-            <input type="text" class="form-control" id="surname" placeholder="Il tuo cognome">
-          </div>
-        </form>  
+          <form action="index.php" method="post">
+          
+            <div class="form-group">
+              <label for="name">Nome</label>
+              <input type="text" class="form-control" name="nome" id="name" aria-describedby="nameHelp" placeholder="Il tuo nome">
+              <small id="nameHelp" class="form-text text-muted">Inserisci il tuo nome</small>
+            </div>
+          
+            <div class="form-group">
+              <label for="surname">Cognome</label>
+              <input type="text" class="form-control" name="cognome" id="surname" aria-describedby="surnameHelp" placeholder="Il tuo cognome">
+              <small id="surnameHelp" class="form-text text-muted">Inserisci il tuo cognome</small>
+            </div>
+          
+            <div class="form-group">
+              <label for="address">Indirizzo</label>
+              <input type="text" class="form-control" name="indirizzo" id="address" aria-describedby="addressHelp" placeholder="Il tuo indirizzo">
+              <small id="addressHelp" class="form-text text-muted">Inserisci il tuo indirizzo</small>
+            </div>
+
+            <div>
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+          </form>      
     </div>
 
     <!-- Optional JavaScript -->
