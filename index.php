1023d73aac151be1fbe1942f40b6087c028398d4
NicolaScapin <44172919+NicolaScapin@users.noreply.github.com>
2019-03-26 15:28:52 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
 <?php
-
-$nome = (isset($_POST['nome'])) ? $_POST['nome']:'';
-$cognome = (isset($_POST['cognome'])) ? $_POST['cognome']:'';
-$indirzzo = (isset($_POST['indirizzo'])) ? $_POST['indirizzo']:'';
-
+$nome = (isset($_POST['nome'])) ? $_POST['nome'] : '';
+$cognome = (isset($_POST['cognome'])) ? $_POST['cognome'] : '';
+$indirizzo = (isset($_POST['indirizzo'])) ? $_POST['indirizzo'] : '';
 ?>
-
 <!doctype html>
 <html lang="en">
   <head>
@@ -14,51 +11,47 @@ $indirzzo = (isset($_POST['indirizzo'])) ? $_POST['indirizzo']:'';
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
 
-    <title>Tag Form</title>
+    <title>esercizio-php-form</title>
   </head>
-  <body> 
-    <div class="container-fluid">
+  <body>
+    <div class="container">
+      <h1>Inserisci i tuoi dati qui</h1>
       <div class="alert alert-primary" role="alert">
         <?php
-          foreach ($_POST as $key => $value) {
-            print $key . ': ' . $value . '<br>';
-          }
+        '<ul>';
+        foreach ($_POST as $key => $value) {
+          print '<li>' . $key . ': ' . $value;
+        }
+        '</li>';
+        '</ul>';
         ?>
-      </div>
-        <h1>Inserisci i tuoi dati</h1>
-        
-          <form action="index.php" method="post">
-          
-            <div class="form-group">
-              <label for="name">Nome</label>
-              <input type="text" class="form-control" name="nome" id="name" aria-describedby="nameHelp" placeholder="Il tuo nome">
-              <small id="nameHelp" class="form-text text-muted">Inserisci il tuo nome</small>
-            </div>
-          
-            <div class="form-group">
-              <label for="surname">Cognome</label>
-              <input type="text" class="form-control" name="cognome" id="surname" aria-describedby="surnameHelp" placeholder="Il tuo cognome">
-              <small id="surnameHelp" class="form-text text-muted">Inserisci il tuo cognome</small>
-            </div>
-          
-            <div class="form-group">
-              <label for="address">Indirizzo</label>
-              <input type="text" class="form-control" name="indirizzo" id="address" aria-describedby="addressHelp" placeholder="Il tuo indirizzo">
-              <small id="addressHelp" class="form-text text-muted">Inserisci il tuo indirizzo</small>
-            </div>
-
-            <div>
-              <button type="submit" class="btn btn-primary">Submit</button>
-            </div>
-          </form>      
+        </div>
+        <form action="index.php" method="post">
+          <div class="form-group">
+            <label for="name">Nome</label>
+            <input type="text" class="form-control" name="nome" id="name" placeholder="Il tuo nome">
+            <small class="form-text text-muted">Scrivi il tuo nome</small>
+          </div>
+          <div class="form-group">
+            <label for="surname">Cognome</label>
+            <input type="text" class="form-control" name="cognome" id="surname" placeholder="Il tuo cognome">
+            <small class="form-text text-muted">Scrivi il tuo cognome</small>
+          </div>
+          <div class="form-group">
+            <label for="indirizzo">E-mail</label>
+            <input type="text" class="form-control" name="indirizzo" id="indirizzo" placeholder="Il tuo indirizzo e-mail">
+            <small class="form-text text-muted">Scrivi la tua e-mail</small>
+          </div>
+          <button type="submit" class="btn btn-primary">Iscriviti</button>
+        </form>
     </div>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="js/jquery-3.3.1.min.js"></script>
-    <script src="js/popper.min.js"></script>
-    <script src=js/bootstrap.min.js"></script>
+    <script src="js/jquery-3.3.1.slim.min.js"> </script>
+    <script src="js/popper.min.js"> </script>
+    <script src="js/bootstrap.min.js"> </script>
   </body>
 </html>
